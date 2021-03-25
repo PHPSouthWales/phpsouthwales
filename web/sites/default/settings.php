williamcameron/phpsouthwales-uk

Use Drupal config for Meetup environment variables
Rather than calling the environment variables directly within the code,
add a configuration schema for the `event_pull` module and inject the
environment variables via Drupal's configuration system and the
settings.php file.

Fixes #198

web/sites/default/settings.php
@@ -48,3 +48,7 @@ if (file_exists($app_root . '/' . $site_path . '/settings.platformsh.php')) {
 if (file_exists($app_root . '/' . $site_path . '/settings.local.php')) {
   include $app_root . '/' . $site_path . '/settings.local.php';
 }
+
+// event_pull settings.
+$config['event_pull.config']['meetup_event_statuses'] = env('MEETUP_EVENT_STATUSES');
+$config['event_pull.config']['meetup_group_url_name'] = env('MEETUP_GROUP_URL_NAME');

PHPSouthWales/phpsouthwales

web/sites/default/settings.php
@@ -39,6 +39,10 @@ if (getenv('APP_ENV') == 'ci') {
 // This is defined inside the read-only "config" directory, deployed via Git.
 $settings['config_sync_directory'] = '../config';
 
+// event_pull settings.
+$config['event_pull.config']['meetup_event_statuses'] = getenv('MEETUP_EVENT_STATUSES');
+$config['event_pull.config']['meetup_group_url_name'] = getenv('MEETUP_GROUP_URL_NAME');
+
 // Automatic Platform.sh settings.
 if (file_exists($app_root . '/' . $site_path . '/settings.platformsh.php')) {
   include $app_root . '/' . $site_path . '/settings.platformsh.php';